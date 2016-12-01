c758ec33646666b00747b2d5c658d6089623af22
Binayak Gouri Shankar <binayakgourishankar@gmail.com>
2016-12-01 23:59:53 +0530

    This is File for an array  REPO

---
+++ arr.java
@@ -0,0 +1,16 @@
+import java.util.*;
+classArrayExample1{
+public static void main(String[] args)
+{ int[]array=new int [10];
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 10 no.");
+for(int i=0;i<=10;i++){
+arr[i]=sc.nextInt();}
+
+
+for(int i=0;i<5;i++)
+{System.out.println("arr["+i+"]="+arr[i]);
+}
+
+}
+sc.close();
